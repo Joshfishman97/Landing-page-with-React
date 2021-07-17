9bc10c641e5685e965465498f2b2b43faddc4adc
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-17 16:57:17 +0000

    another commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,17 +1,14 @@
 import React from "react";
-import {Navbar} from  "./Navbar"
+import { Navbar } from "./Navbar";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Navbar() {
-    return (
-      <div>
-
-	<Navbar />		
-      </div>
-		
+export function Home() {
+	return (
+		<div>
+			<Navbar />
+		</div>
 	);
 }
-ReactDOM.render(<Home />, document.querySelector("div"));
